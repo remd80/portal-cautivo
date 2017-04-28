738083260d4533c3a3b8e60f72d93df8cde131e6
remd80 <robertom@tecniservicios.net>
2017-04-27 18:50:23 -0600

    Codigos JavaScript para Validar el Ticket del Voucher

---
+++ captiveportal-validator2.js
@@ -0,0 +1,14 @@
+<!--
+	
+	var validator2 = function() {
+		var ticket = document.getElementById("voucher").value;
+		
+		if (ticket.length == 0) {
+			alert("Por favor ingrese su Ticket!");
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+//-->
